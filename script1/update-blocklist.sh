fc385fe580076db2dd5c1e439aad4a5cd6942327
Jason <jasonyan1169@gmail.com>
2025-07-13 20:07:07 -0700

    updated script1 - took out the temp path and wrote sudo for the threat list pull updated script2 - added check_rule in setup, added spaces in the comments of update-blocklist

--- script1/update-blocklist.sh
+++ script1/update-blocklist.sh
@@ -3,13 +3,14 @@
 # FireHOL level 1 blocklist (low false positives)
 BLOCKLIST_URL="https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level1.netset"
 BLOCKLIST_PATH="/etc/pf.blocklist"
+TEMP_PATH="/tmp/pf.blocklist.tmp"
 
 #checks if /etc/pf.blocklist exists, if not creates it
 sudo touch /etc/pf.blocklist
 
 # Fetch the list and filter for valid IPs (in case there are comments or garbage)
 # and outputs the IPs to the blocklist file
-curl -s "$BLOCKLIST_URL" -o "$BLOCKLIST_PATH"
+sudo curl -s "$BLOCKLIST_URL" -o "$BLOCKLIST_PATH"
 
 # Replace pf table entries with new list
 sudo pfctl -t bad_ips -T replace -f /etc/pf.blocklist
--- script2/setup-pf-conf.sh
+++ script2/setup-pf-conf.sh
@@ -1,6 +1,25 @@
 #The following script is used to add the following lines to the /etc/pf.conf file
 #!/bin/bash
 
-echo 'anchor "blockips"' | sudo tee -a /etc/pf.conf > /dev/null
-echo 'load anchor "blockips" from "/etc/pf.anchors/blockips"' | sudo tee -a /etc/pf.conf > /dev/null
+#The following script is used to add the following lines to the /etc/pf.conf file
+
+#!/bin/bash
+
+PF_CONF="/etc/pf.conf"
+
+#check_rule checks if the rule exists in the pf.conf file and adds it if not
+check_rule() {
+    local rule="$1"
+    if sudo grep -Fqx "$rule" "$PF_CONF"; then
+        #used for debugging
+        #echo "Found: $rule"
+        return 0
+    else
+        echo "$rule" | sudo tee -a $PF_CONF > /dev/null
+        return 1
+    fi
+}
+
+check_rule 'anchor "blockips"'
+check_rule 'load anchor "blockips" from "/etc/pf.anchors/blockips"'
 
