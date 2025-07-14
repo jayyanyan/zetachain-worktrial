fdb0eaa83b627ea10b6410585dd1278848e4cbb8
Jason <jasonyan1169@gmail.com>
2025-07-13 20:45:52 -0700

    refined the check for blocklist_path

--- script1/update-blocklist.sh
+++ script1/update-blocklist.sh
@@ -3,9 +3,12 @@
 # FireHOL level 1 blocklist (low false positives)
 BLOCKLIST_URL="https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level1.netset"
 BLOCKLIST_PATH="/etc/pf.blocklist"
+LOG_FILE="/etc/updatepfblocklist.log"
 
-#checks if /etc/pf.blocklist exists, if not creates it
-sudo touch /etc/pf.blocklist
+# Create /etc/pf.blocklist if it doesn't exist
+if [[ ! -f "$BLOCKLIST_PATH" ]]; then
+  sudo touch "$BLOCKLIST_PATH"
+fi
 
 # Fetch the list and filter for valid IPs (in case there are comments or garbage)
 # and outputs the IPs to the blocklist file
@@ -16,8 +19,8 @@ sudo curl -s $BLOCKLIST_URL \
 
 
 # Replace pf table entries with new list
-sudo pfctl -t bad_ips -T replace -f /etc/pf.blocklist
+sudo pfctl -t bad_ips -T replace -f "$BLOCKLIST_PATH"
 
-sudo touch /etc/updatepfblocklist.log
+sudo touch "$LOG_FILE""
 # Log the update
-echo "$(date): Updated blocklist with $(wc -l < $BLOCKLIST_PATH) IPs" | sudo tee -a /etc/updatepfblocklist.log > /dev/null
+echo "$(date): Updated blocklist with $(wc -l < $BLOCKLIST_PATH) IPs" | sudo tee -a "$LOG_FILE" > /dev/null
