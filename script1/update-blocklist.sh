b328a4e3f45923215d11077a7d0eb962ab3bf1cb
Jason <jasonyan1169@gmail.com>
2025-07-13 18:29:45 -0700

    updating update-blokclist to check for pf.blocklist if not create it, troubleshooting line 15

--- script1/update-blocklist.sh
+++ script1/update-blocklist.sh
@@ -4,12 +4,15 @@
 BLOCKLIST_URL="https://raw.githubusercontent.com/firehol/blocklist-ipsets/master/firehol_level1.netset"
 BLOCKLIST_PATH="/etc/pf.blocklist"
 
+#checks if /etc/pf.blocklist exists, if not creates it
+sudo touch /etc/pf.blocklist
+
 # Fetch the list and filter for valid IPs (in case there are comments or garbage)
 # and outputs the IPs to the blocklist file
-curl -s "$BLOCKLIST_URL" | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' > "$BLOCKLIST_PATH"
+curl -s "$BLOCKLIST_URL" -o "$BLOCKLIST_PATH"
 
 # Replace pf table entries with new list
-sudo pfctl -t bad_ips -T replace -f "$BLOCKLIST_PATH"
+sudo pfctl -t bad_ips -T replace -f /etc/pf.blocklist
 
 # Log the update
 echo "$(date): Updated blocklist with $(wc -l < $BLOCKLIST_PATH) IPs" >> /var/log/ipblock.log
